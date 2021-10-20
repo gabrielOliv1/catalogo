815af872ac0d61798f5011ac3a8166cd4646485f
Gabriel Oliveira <gabrieloliveiravicente2015@gmail.com>
2021-10-19 22:23:39 -0300

    config deploy

--- database/index.js
+++ database/index.js
@@ -1,14 +1,9 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {     
+const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER, process.env.DB_PASS,  {     
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
-  protocol: 'postgres',     
-  dialectOptions: {         
-    ssl: {             
-      require: true,             
-      rejectUnauthorized: false         
-    }     
-  } 
 }) 
 
 module.exports = sequelize;
